cf17d50fd9f0ba9230e661e3e8207096f38c7b2b
Ulas Can Cengiz <ulas@ulascengiz.com>
2026-02-17 16:49:37 +0100

    fix(taps): handle deferred close errors to satisfy errcheck

--- internal/taps/git.go
+++ internal/taps/git.go
@@ -119,13 +119,13 @@ func copyFile(src, dst string) error {
 	if err != nil {
 		return err
 	}
-	defer in.Close()
+	defer func() { _ = in.Close() }()
 
 	out, err := os.Create(dst)
 	if err != nil {
 		return err
 	}
-	defer out.Close()
+	defer func() { _ = out.Close() }()
 
 	_, err = io.Copy(out, in)
 	return err
